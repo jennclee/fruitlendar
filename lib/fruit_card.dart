d0fcb43648b1215950dafe1320df8600e95dae2f
Jennifer Lee <8753352+jennclee@users.noreply.github.com>
2020-06-04 22:08:54 -0700

    Fix child widget nesting errors

--- lib/fruit_card.dart
+++ lib/fruit_card.dart
@@ -19,7 +19,7 @@ class FruitCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 200,
+      height: 250,
       width: 200,
       child: SafeArea(
         child: CupertinoButton(
@@ -27,55 +27,50 @@ class FruitCard extends StatelessWidget {
             direction: FlipDirection.HORIZONTAL,
             front: Column(
               children: [
-                Expanded(
-                  child: Container(
-                    alignment: Alignment.center,
-                    height: 150,
-                    width: 200,
-                    child: Image.asset(
-                      fruit.image,
-                    ),
-                    decoration: BoxDecoration(
-                      color: Colors.amber[50],
-                      borderRadius: BorderRadius.only(
-                          topLeft: Radius.circular(12),
-                          topRight: Radius.circular(12)),
-                    ),
+                Container(
+                  alignment: Alignment.center,
+                  height: 100,
+                  width: 200,
+                  child: Image.asset(
+                    fruit.image,
+                  ),
+                  decoration: BoxDecoration(
+                    color: Colors.amber[50],
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(12),
+                        topRight: Radius.circular(12)),
                   ),
                 ),
-                Expanded(
-                  child: Container(
-                    height: 25,
-                    alignment: Alignment.center,
-                    decoration: BoxDecoration(
-                      color: Colors.amber[50],
-                      borderRadius: BorderRadius.only(
-                          bottomLeft: Radius.circular(12),
-                          bottomRight: Radius.circular(12)),
-                    ),
-                    child: Text(
-                      fruit.name,
-                      style: TextStyle(
-                          color: Colors.black87,
-                          fontSize: 20,
-                          fontWeight: FontWeight.w600),
-                    ),
+                Container(
+                  height: 35,
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                    color: Colors.amber[50],
+                    borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(12),
+                        bottomRight: Radius.circular(12)),
+                  ),
+                  child: Text(
+                    fruit.name,
+                    style: TextStyle(
+                        color: Colors.black87,
+                        fontSize: 20,
+                        fontWeight: FontWeight.w600),
                   ),
                 ),
               ],
             ),
-            back: Expanded(
-              child: Container(
-                alignment: Alignment.center,
-                decoration: BoxDecoration(
-                  color: Colors.amber[50],
-                  borderRadius: BorderRadius.circular(12),
-                ),
-                child: Text(
-                  'back!',
-                  style: TextStyle(
-                    color: Colors.black87,
-                  ),
+            back: Container(
+              padding: const EdgeInsets.all(10),
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                color: Colors.amber[50],
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: Text(
+                fruit.info,
+                style: TextStyle(
+                  color: Colors.black87,
                 ),
               ),
             ),
