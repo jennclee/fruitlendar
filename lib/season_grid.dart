8e2bac46c952e8447af47dfc1ee478d213a6e043
Jennifer Lee <8753352+jennclee@users.noreply.github.com>
2020-06-04 20:17:01 -0700

    Utilize multiple seasons fixture

--- lib/season_grid.dart
+++ lib/season_grid.dart
@@ -1,8 +1,10 @@
+import 'dart:async' show Future;
+import 'dart:convert';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart' show rootBundle;
 import 'image_card.dart';
 import 'models/season.dart';
-import 'seasons.dart';
 
 class SeasonGrid extends StatefulWidget {
   @override
@@ -10,7 +12,7 @@ class SeasonGrid extends StatefulWidget {
 }
 
 class SeasonGridState extends State<SeasonGrid> {
-  Future _season;
+  Future<List<Season>> _season;
 
   @override
   void initState() {
@@ -19,23 +21,33 @@ class SeasonGridState extends State<SeasonGrid> {
     _season = loadSeasons();
   }
 
+  Future<List<Season>> loadSeasons() async {
+    String jsonString = await rootBundle.loadString('fixtures/seasons.json');
+    List<dynamic> jsonResponse = json.decode(jsonString);
+    List<Season> seasons = [];
+    for (var i = 0; i < jsonResponse.length; i++) {
+      seasons.add(new Season.fromJson(jsonResponse[i]));
+    }
+    return seasons;
+  }
+
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
       builder: (context, snapshot) {
         if (snapshot.hasData) {
-          Season season = snapshot.data;
+          List<Season> seasons = snapshot.data;
           return Container(
             child: Column(
               children: [
                 Row(children: [
-                  Expanded(child: new ImageCard(season)),
-                  Expanded(child: new ImageCard(season)),
+                  Expanded(child: new ImageCard(seasons[0])),
+                  Expanded(child: new ImageCard(seasons[1])),
                 ]),
                 Row(
                   children: [
-                    Expanded(child: new ImageCard(season)),
-                    Expanded(child: new ImageCard(season)),
+                    Expanded(child: new ImageCard(seasons[2])),
+                    Expanded(child: new ImageCard(seasons[3])),
                   ],
                 ),
               ],
