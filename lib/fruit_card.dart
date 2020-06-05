c82794760dc097800f071390e874f836282f16b2
Jennifer Lee <8753352+jennclee@users.noreply.github.com>
2020-06-04 22:01:47 -0700

    Implement fruit card flipping

--- lib/fruit_card.dart
+++ lib/fruit_card.dart
@@ -1,11 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flip_card/flip_card.dart';
 import 'models/fruit.dart';
 
 class FruitCard extends StatelessWidget {
   final Fruit fruit;
 
   FruitCard(this.fruit);
+
+  BoxDecoration basicCard() {
+    return BoxDecoration(
+      color: Colors.amber[50],
+      borderRadius: BorderRadius.only(
+          bottomLeft: Radius.circular(12), bottomRight: Radius.circular(12)),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,43 +23,62 @@ class FruitCard extends StatelessWidget {
       width: 200,
       child: SafeArea(
         child: CupertinoButton(
-          child: Column(
-            children: [
-              Expanded(
-                child: Container(
-                  height: 150,
-                  width: 200,
-                  child: Image.asset(
-                    fruit.image,
+          child: FlipCard(
+            direction: FlipDirection.HORIZONTAL,
+            front: Column(
+              children: [
+                Expanded(
+                  child: Container(
+                    alignment: Alignment.center,
+                    height: 150,
+                    width: 200,
+                    child: Image.asset(
+                      fruit.image,
+                    ),
+                    decoration: BoxDecoration(
+                      color: Colors.amber[50],
+                      borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(12),
+                          topRight: Radius.circular(12)),
+                    ),
                   ),
-                  decoration: BoxDecoration(
-                    color: Colors.amber[50],
-                    borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(12),
-                        topRight: Radius.circular(12)),
+                ),
+                Expanded(
+                  child: Container(
+                    height: 25,
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                      color: Colors.amber[50],
+                      borderRadius: BorderRadius.only(
+                          bottomLeft: Radius.circular(12),
+                          bottomRight: Radius.circular(12)),
+                    ),
+                    child: Text(
+                      fruit.name,
+                      style: TextStyle(
+                          color: Colors.black87,
+                          fontSize: 20,
+                          fontWeight: FontWeight.w600),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            back: Expanded(
+              child: Container(
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                  color: Colors.amber[50],
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: Text(
+                  'back!',
+                  style: TextStyle(
+                    color: Colors.black87,
                   ),
                 ),
               ),
-              Expanded(
-                child: Container(
-                  height: 25,
-                  alignment: Alignment.bottomCenter,
-                  decoration: BoxDecoration(
-                    color: Colors.amber[50],
-                    borderRadius: BorderRadius.only(
-                        bottomLeft: Radius.circular(12),
-                        bottomRight: Radius.circular(12)),
-                  ),
-                  child: Text(
-                    fruit.name,
-                    style: TextStyle(
-                        color: Colors.black87,
-                        fontSize: 20,
-                        fontWeight: FontWeight.w600),
-                  ),
-                ),
-              ),
-            ],
+            ),
           ),
           onPressed: () {
             print('Clicked fruit!');
