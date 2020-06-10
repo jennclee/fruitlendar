fb7c36906fee6db15baf41d34885d213285fffc0
Jennifer Lee <8753352+jennclee@users.noreply.github.com>
2020-06-09 19:32:16 -0700

    Move season data retrieval up one level
    * Use data dict instead of list

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,11 @@
+import 'dart:async' show Future;
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/services.dart' show rootBundle;
+
+import 'models/season.dart';
 import 'hero.dart';
 import 'season_grid.dart';
 import 'season_page.dart';
@@ -31,20 +37,59 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+class MyHomePage extends StatefulWidget {
   final String title;
+
+  const MyHomePage({Key key, this.title}) : super(key: key);
+
+  @override
+  MyHomePageState createState() => MyHomePageState();
+}
+
+class MyHomePageState extends State<MyHomePage> {
+  final String title;
+
+  MyHomePageState({this.title});
+  Future<Map<String, Season>> _seasons;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _seasons = loadSeasons();
+  }
+
+  Future<Map<String, Season>> loadSeasons() async {
+    String jsonString =
+        await rootBundle.loadString('lib/fixtures/seasonsDict.json');
+    Map<String, dynamic> jsonResponse = json.decode(jsonString);
+    Map<String, Season> seasons = {};
+    jsonResponse
+        .forEach((key, value) => {seasons[key] = new Season.fromJson(value)});
+    return seasons;
+  }
 
   @override
   Widget build(BuildContext context) {
-    return CupertinoPageScaffold(
-      navigationBar: CupertinoNavigationBar(
-        middle: Text(this.title, style: TextStyle(color: Colors.black87)),
-      ),
-      child: Column(children: [
-        heroSection,
-        SeasonGrid(),
-      ]),
+    return FutureBuilder(
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          Map<String, Season> seasons = snapshot.data;
+          return CupertinoPageScaffold(
+            navigationBar: CupertinoNavigationBar(
+              middle:
+                  Text(widget.title, style: TextStyle(color: Colors.black87)),
+            ),
+            child: Column(children: [
+              heroSection,
+              SeasonGrid(seasons),
+            ]),
+          );
+        } else {
+          return Center(child: CircularProgressIndicator());
+        }
+      },
+      future: _seasons,
     );
   }
 }
--- lib/season_grid.dart
+++ lib/season_grid.dart
@@ -1,63 +1,30 @@
-import 'dart:async' show Future;
-import 'dart:convert';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart' show rootBundle;
 import 'image_card.dart';
 import 'models/season.dart';
 
-class SeasonGrid extends StatefulWidget {
-  @override
-  SeasonGridState createState() => SeasonGridState();
-}
+class SeasonGrid extends StatelessWidget {
+  final Map<String, Season> seasons;
 
-class SeasonGridState extends State<SeasonGrid> {
-  Future<List<Season>> _season;
-
-  @override
-  void initState() {
-    super.initState();
-
-    _season = loadSeasons();
-  }
-
-  Future<List<Season>> loadSeasons() async {
-    String jsonString = await rootBundle.loadString('fixtures/seasons.json');
-    List<dynamic> jsonResponse = json.decode(jsonString);
-    List<Season> seasons = [];
-    for (var i = 0; i < jsonResponse.length; i++) {
-      seasons.add(new Season.fromJson(jsonResponse[i]));
-    }
-    return seasons;
-  }
+  SeasonGrid(this.seasons);
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-      builder: (context, snapshot) {
-        if (snapshot.hasData) {
-          List<Season> seasons = snapshot.data;
-          return Container(
-            child: Column(
-              children: [
-                Row(children: [
-                  Expanded(child: new ImageCard(seasons[0])),
-                  Expanded(child: new ImageCard(seasons[1])),
-                ]),
-                Row(
-                  children: [
-                    Expanded(child: new ImageCard(seasons[2])),
-                    Expanded(child: new ImageCard(seasons[3])),
-                  ],
-                ),
-              ],
-            ),
-          );
-        } else {
-          return Center(child: CircularProgressIndicator());
-        }
-      },
-      future: _season,
+    return Container(
+      child: Column(
+        children: [
+          Row(children: [
+            Expanded(child: new ImageCard(seasons['Spring'])),
+            Expanded(child: new ImageCard(seasons['Summer'])),
+          ]),
+          Row(
+            children: [
+              Expanded(child: new ImageCard(seasons['Autumn'])),
+              Expanded(child: new ImageCard(seasons['Winter'])),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
