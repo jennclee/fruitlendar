5efbb907de2b190d6a99aa71c801affc6a333112
Jennifer Lee <8753352+jennclee@users.noreply.github.com>
2020-06-10 22:51:06 -0700

    Switch to icons for seasons

--- lib/fixtures/season_repository.dart
+++ lib/fixtures/season_repository.dart
@@ -4,25 +4,25 @@ class SeasonRepository {
   static const _allSeasons = <Season>[
     Season(
       name: "Spring",
-      image: "images/spring-cherry-blossom-unsplash.jpg",
+      image: "images/tulip.png",
       description: "Spring brings the beginning of stone fruit season.",
       monthsNA: [3, 4, 5],
     ),
     Season(
       name: "Summer",
-      image: "images/summer-border-unsplash.jpg",
+      image: "images/coconut-tree.png",
       description: "Summer brings berry season",
       monthsNA: [6, 7, 8],
     ),
     Season(
       name: "Autumn",
-      image: "images/autumn-trees-unsplash.jpg",
+      image: "images/leaves.png",
       description: "Hearty fruits show up in Autumn",
       monthsNA: [9, 10, 11],
     ),
     Season(
       name: "Winter",
-      image: "images/winter-mountain-unsplash.jpg",
+      image: "images/snowflake.png",
       description: "Citrus fruits are great during Winter",
       monthsNA: [12, 1, 2],
     ),
